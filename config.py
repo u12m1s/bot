afb2cb73c2907d9b3e9716c044ae6ce00066c3f5
ulmas005 <o.xo'jaev@student.inha.uz>
2025-08-12 21:46:22 +0500

    init

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+# Загружаем переменные из .env, если они есть
+load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+
 if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN is not set in environment variables.")
+    raise ValueError("❌ BOT_TOKEN не найден! Установите его в переменные окружения или в .env файл.")
+
